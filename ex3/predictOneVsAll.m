bdfba91af7219fecf880857304d9ee212b3d220a
Tom "Ravi" Hale <tom@hale.ee>
2017-06-16 20:05:41 +0700

    Complete predictOneVsAll.m

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -9,13 +9,18 @@ function p = predictOneVsAll(all_theta, X)
 %  for 4 examples) 
 
 m = size(X, 1);
-num_labels = size(all_theta, 1);
+% num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+% p = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
+
+% Each row contains K sigmoids
+sigmoids = sigmoid (X * all_theta');
+
+[_, p] = max(sigmoids, [], 2);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
