9e50533578293d6a03ff9e1f06a8fbd71e7c4428
Tom "Ravi" Hale <tom@hale.ee>
2017-07-31 21:57:59 +0700

    Return z and activations

--- assignments/ex4/predict.m
+++ assignments/ex4/predict.m
@@ -1,14 +1,14 @@
-function [p, activation] = predict(Theta1, Theta2, X)
+function [hX, z_output, activation] = predict(Theta1, Theta2, X)
 %PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted output layer activation
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted output layer output
 %   trained weights of a neural network (Theta1, Theta2)
 
 % Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+% m = size(X, 1);
+% num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+% p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -22,24 +22,25 @@ p = zeros(size(X, 1), 1);
 %
 
 
-allTheta{1} = Theta1;
-allTheta{2} = Theta2;
+% Allow generalisation to many Theta matrices
+Theta{1} = Theta1;
+Theta{2} = Theta2;
 
 % size(X) == 5000 * 401 -- 5000 examples, 400 features
 % size(Theta1) == 25 x 401  -- 25 nodes in hidden layer
 % size(A1) == 5000 x 26  -- 25 hidden nodes + 1 bias
 % size(Theta2) == 10 x 26 -- 10 class output
 
-activation{1} = X; % Input layer
-
-for Theta = 1:size(allTheta,2) % Cell array layout is like row vector
-% Add bias node
-	bias_added = [ones(rows(activation{Theta}), 1) activation{Theta}];
-
-	activation{Theta + 1} = sigmoid(bias_added * allTheta{Theta}');
+z_output{1} = activation{1} = X; % Input layer
+for layer = 2:length(Theta) + 1
+  % Add bias node to previous layer
+  input = [ones(rows(activation{layer-1}), 1) activation{layer-1}];
+  z_output{layer} = input * Theta{layer-1}';
+  activation{layer} = sigmoid(z_output{layer});
+  % describe activation{layer}
 end
 
-p = activation{end};
+hX = activation{end};
 
 % =========================================================================
 
